44c438cc1238926a7d1d70ebe8252b24d1261bf8
leonardo-assertech <leonardo-rezende@assertech.app>
2023-01-18 00:27:18 -0300

    Feat: desacoplagem das rotas do arquivo server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,16 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from ".prisma/client";
+import { appRoutes } from "./routes";
 
-const prisma = new PrismaClient();
 const app = Fastify();
 
 app.register(cors);
 
-app.get('', async () => {
-    const habits = await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: 'Beber'
-            }
-        }
-    })
-});
+app.register(appRoutes);
 
 app.listen({
     port: 3333
-});
+})
+.then(()=>{
+    console.log('running')
+})
