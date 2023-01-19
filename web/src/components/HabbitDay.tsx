18cc35fc899a3e6e9b4b07e159a2b53b2d9c795a
leonardo-assertech <leonardo-rezende@assertech.app>
2023-01-19 00:25:35 -0300

    Feat: criação do habbit day para visualizar habitos completos nauqle dia

---
+++ web/src/components/HabbitDay.tsx
@@ -0,0 +1,10 @@
+
+
+function HabbitDay() {
+    return (
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-900 rounded-lg">
+            
+        </div>
+    )
+}
+export default HabbitDay
