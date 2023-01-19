9a01fa80c7289dedcb10647b7db0b6562ec64002
leonardo-assertech <leonardo-rezende@assertech.app>
2023-01-19 00:25:16 -0300

    Feat: adição de components de header e summary table

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,16 @@
-import Habbit from "./components/Habbit"
+import Header from "./components/Header"
+import SummaryTable from "./components/SummaryTable"
+
 
 function App() {
 
   return (
-    <>
-      <Habbit completed={10} />
-      <Habbit completed={20} />
-      <Habbit completed={30} />
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
 
